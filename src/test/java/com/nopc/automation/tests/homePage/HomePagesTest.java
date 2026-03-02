b7ba046e2bf268e652013360fc5ed92840a94966
Reddy <2464178@cognizant.com>
2026-03-02 16:37:11 +0530

    implemented product details and Homepage tests validations

--- src/test/java/com/nopc/automation/tests/homePage/HomePagesTest.java
+++ src/test/java/com/nopc/automation/tests/homePage/HomePagesTest.java
@@ -1,5 +1,15 @@
 package com.nopc.automation.tests.homePage;
 
-public class HomePagesTest {
-    //Type code
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.nopc.automation.constants.AppConstants;
+import com.nopc.automation.core.driver.DriverManager;
+import com.nopc.automation.tests.base.BaseTest;
+
+public class HomePagesTest extends BaseTest {
+    @Test(description="TC11 – Verify homepage loads successfully")
+    public void tc11_homepage_loads(){
+        String title = DriverManager.getDriver().getTitle();
+        Assert.assertTrue(title.contains(AppConstants.HOME_TITLE));
+    }
 }
